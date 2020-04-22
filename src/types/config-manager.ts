98627f05252cdf14a6c1bf36e6d1a88268d6f6ee
NuclearRedeye <j.summerfield@elifesciences.org>
2020-04-22 11:26:37 +0100

    fix: copy and paste error in type definition

--- src/types/config-manager.ts
+++ src/types/config-manager.ts
@@ -4,5 +4,4 @@ export interface ConfigManager {
   get(key: string): string;
   set(key: string, value: string, overwrite?: boolean): void;
   apply(config: Config): void;
-  apply(config: Config): void;
 }
