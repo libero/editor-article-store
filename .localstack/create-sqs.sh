4cce161ab4f39d2be8f29859bcbfbc8398cb1ff5
Cory <c.donkin@elifesciences.org>
2020-10-09 09:06:48 +0100

    WIP:begin trying to configure s3 notifications

--- .localstack/create-sqs.sh
+++ .localstack/create-sqs.sh
@@ -1,3 +1,6 @@
 echo 'creating SQS queue'
 awslocal sqs create-queue --queue-name KryiaUploadQueue
+pwd
+ls
+awslocal s3api put-bucket-notification-configuration --bucket kryia --notification-configuration file://notification.json
 echo 'SQS queue created'
