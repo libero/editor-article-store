b6db35fa6a8e90576ff6871541768ccb435fa5ce
Cory <c.donkin@elifesciences.org>
2020-10-12 14:21:40 +0100

    add README for file watcher

--- file-watcher/index.js
+++ file-watcher/index.js
@@ -4,21 +4,21 @@ const fs = require('fs');
 const nodePath = require('path');
 
 const s3 = new AWS.S3({ 
-  endpoint: 'http://localstack:4566', 
+  endpoint: process.env.AWS_ENDPOINT, 
   apiVersion: '2006-03-01',
   accessKeyId: 'accessKeyId',
   secretAccessKey: 'secretAccessKey', 
   s3ForcePathStyle: true 
 });
 const watcher = chokidar.watch('/bucket-to-watch');
-console.log('listening for files and mapping to bucket: ' + process.env.S3BUCKET);
+console.log('listening for files and mapping to bucket: ' + process.env.S3_BUCKET);
 watcher
   .on('add', path => {
     console.log(`File ${path} has been added`);
     const fileContent = fs.readFileSync(path);
     var params = {
       Body: fileContent, 
-      Bucket: process.env.S3BUCKET, 
+      Bucket: process.env.S3_BUCKET, 
       Key: nodePath.basename(path)
      };
      s3.putObject(params, function(err, data) {
