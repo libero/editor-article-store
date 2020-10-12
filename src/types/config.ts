c73362800baa7be464d830e333f2a07570aca761
noris alsaadie <>
2020-10-12 11:18:43 +0100

    Update config type

--- src/types/config.ts
+++ src/types/config.ts
@@ -1,4 +1,9 @@
 export type Config = {
   articleRoot?: string;
   port?: number;
+  awsSqsRegion?: string;
+  awsSqsAccessKey?: string;
+  awsSqsSecretAccessKey?: string;
+  sqsQueueUrl?: string;
+  awsRegion?: string;
 };
