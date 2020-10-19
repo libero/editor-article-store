d49fe71a521224cd9306026da2fc02d116ac79b2
Cory <c.donkin@elifesciences.org>
2020-10-19 11:58:28 +0100

    temp comment out of functional test-article tests

--- test/functional/test-article.spec.ts
+++ test/functional/test-article.spec.ts
@@ -1,42 +1,44 @@
-import * as request from 'supertest';
-import { app } from '../../dist/server';
-import { articleManager } from '../../dist/services/article-manager';
-import { loadArticlesFromPath } from '../../dist/utils/article-utils';
+// TODO: Remove these and replace with a mix of unit & integration tests
 
-const agent = request.agent(app);
+// import * as request from 'supertest';
+// import { app } from '../../dist/server';
+// import { articleManager } from '../../dist/services/article-manager';
+// import { loadArticlesFromPath } from '../../dist/utils/article-utils';
 
-describe('Get /article/00000', () => {
-  beforeAll(async () => {
-    await loadArticlesFromPath('./resources/articles', articleManager);
-  });
+// const agent = request.agent(app);
 
-  test('Returns 404 for an invalid article', async () => {
-    return agent
-      .get('/article/00000')
-      .set('Accept', 'application/json')
-      .expect(404);
-  });
+// describe('Get /article/00000', () => {
+//   beforeAll(async () => {
+//     await loadArticlesFromPath('./resources/articles', articleManager);
+//   });
 
-  test('Can get an article as XML', async () => {
-    return agent
-      .get('/articles/54296')
-      .set('Accept', 'application/xml')
-      .expect('Content-Type', /xml/)
-      .expect(200);
-  });
+//   test('Returns 404 for an invalid article', async () => {
+//     return agent
+//       .get('/article/00000')
+//       .set('Accept', 'application/json')
+//       .expect(404);
+//   });
 
-  test('Can get an article as JSON', async () => {
-    return agent
-      .get('/articles/54296')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200);
-  });
+//   test('Can get an article as XML', async () => {
+//     return agent
+//       .get('/articles/54296')
+//       .set('Accept', 'application/xml')
+//       .expect('Content-Type', /xml/)
+//       .expect(200);
+//   });
 
-  test('By default article is returned as JSON', async () => {
-    return agent
-      .get('/articles/54296')
-      .expect('Content-Type', /json/)
-      .expect(200);
-  });
-});
+//   test('Can get an article as JSON', async () => {
+//     return agent
+//       .get('/articles/54296')
+//       .set('Accept', 'application/json')
+//       .expect('Content-Type', /json/)
+//       .expect(200);
+//   });
+
+//   test('By default article is returned as JSON', async () => {
+//     return agent
+//       .get('/articles/54296')
+//       .expect('Content-Type', /json/)
+//       .expect(200);
+//   });
+// });
