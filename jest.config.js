0129ef080bde6a29bdfe0b48c2ea84adaf112d0a
noris alsaadie <>
2020-10-20 17:38:05 +0100

    restore branch check

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   "coverageDirectory": "./test/report",
   "coverageThreshold": {
       "global": {
-          "branches": 75,
+          "branches": 80,
           "functions": 80,
           "lines": 80,
           "statements": 0
