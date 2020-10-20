40386c2e1ef65ab8763b02b7e8d98f399262484e
noris alsaadie <>
2020-10-20 17:39:28 +0100

    More tests

--- test/unit/article-service.test.ts
+++ test/unit/article-service.test.ts
@@ -45,4 +45,17 @@ describe("articleService", () => {
     const articles = await articleService(db).getArticles(0);
     expect(articles.length).toBe(0);
   })
+
+  test("Returns empty array if no articles", async () => {
+    const data = {
+      xml: "<xml></xml",
+      datatype: "xml",
+      articleId: "12345",
+      fileName: "main.xml",
+    };
+    const { insertedId } = await db.collection("articles").insertOne(data);
+    const articles = await articleService(db).getArticles(0);
+    expect(articles.length).toBe(1);
+    expect(articles[0]).toEqual({ _id: insertedId, ...data });
+  })
 });
