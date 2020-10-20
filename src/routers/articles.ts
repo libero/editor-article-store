d192d226819c24f947ebd03b84e9acc8775a0c83
noris alsaadie <>
2020-10-20 11:44:53 +0100

    fix functional tests

--- src/routers/articles.ts
+++ src/routers/articles.ts
@@ -1,28 +1,39 @@
-import { default as express } from 'express';
-import path from 'path';
-import { logRequest } from '../middlewares/log-request';
+import { default as express } from "express";
+import { http415Response } from "../providers/errors";
+
+import { logRequest } from "../middlewares/log-request";
 
 export default (articleService: any): express.Router => {
   const router = express.Router();
 
   // Log all requests on this route.
   router.use(logRequest);
-  
+
   // Get a list of all the available articles.
-  router.get('/', async (req, res) => {
-    const articles = await articleService.getArticles(req.query.page || 0);
-    res
-      .type("application/json")
-      .status(200)
-      .json({ articles });
-  });
-  
+  router.get(
+    "/",
+    async (req, res, next) => {
+      const accept = req.headers.accept || "*/*";
+      if (accept.includes("application/json") || accept.includes("*/*")) {
+        // todo: address as part of the depedency injection work
+        const articles = await articleService.getArticles(req.query.page || 0);
+        res
+          .type("application/json")
+          .status(200)
+          .json({ articles });
+      } else {
+        next();
+      }
+    },
+    http415Response
+  );
+
   // Gets the specified article.
-  router.get('/:articleId', async (req, res) => {
+  router.get("/:articleId", async (req, res) => {
     const accept = req.headers.accept || "";
     const articleId = req.params.articleId;
 
-    const article = await articleService.findByArticleId(articleId); 
+    const article = await articleService.findByArticleId(articleId);
 
     if (!article) {
       res.sendStatus(404);
@@ -43,4 +54,4 @@ export default (articleService: any): express.Router => {
   });
 
   return router;
-} 
+};
