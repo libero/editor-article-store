f532d9631f3627c84742478ff070dc082416c15a
noris alsaadie <>
2020-10-22 12:29:22 +0100

    only convert tif for now

--- src/listeners/convert-image.ts
+++ src/listeners/convert-image.ts
@@ -1,7 +1,16 @@
 import sharp from "sharp";
 import FileType from "file-type";
 
+const supportedMimeTypes = ['image/tif']; // extend as needed
+
 export default async function convert(buffer: Buffer) {
+  const mimeType = await FileType.fromBuffer(buffer);
+  if (supportedMimeTypes.includes(mimeType?.mime as string)) {
+      return {
+        buffer: null,
+        contentType: null,
+      }
+  }
   const jpgBuffer = await sharp(buffer)
     .toFormat("jpg")
     .toBuffer();
