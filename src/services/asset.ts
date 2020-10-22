0a43bc93defd42d75da7cae6b9ef378544e8efe9
Cory <c.donkin@elifesciences.org>
2020-10-22 16:38:20 +0100

    temp fix to get assets serving correctly

--- src/services/asset.ts
+++ src/services/asset.ts
@@ -4,9 +4,11 @@ import { configManager } from "./config-manager";
 export default (s3: S3, config: typeof configManager) => ({
   getAssetUrl: (articleId: string, fileKey: string): string => {
     return s3.getSignedUrl('getObject', {
-      Bucket: config.get('editorS3Bucket'),
-      Key: `${articleId}/${fileKey}`,
-      Expires: 3600
-  })
+        Bucket: config.get('editorS3Bucket'),
+        Key: `${articleId}/${fileKey}`,
+        Expires: 3600
+    })
+    //TODO: remove this. Used for local dev and should be replaced with some kind of proxy.
+    .replace('localstack', 'localhost');
   }
 });
