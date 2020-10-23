9ff458af0564506d0b442d36deb7b396a4c8f4d2
noris alsaadie <>
2020-10-23 09:16:02 +0100

    Add missing file

---
+++ test/unit/asset-service.test.ts
@@ -0,0 +1,20 @@
+import { S3 } from "aws-sdk";
+import assetService from "../../src/services/asset";
+import { ConfigManagerInstance } from "../../src/services/config-manager";
+
+describe("assetService", () => {
+  test("Should call S3 with the correct key and bucket", async () => {
+    const mockS3 = {
+      getSignedUrl: jest.fn().mockReturnValue("http://mock"),
+    };
+    assetService(
+      (mockS3 as unknown) as S3,
+      ({ get: () => "editorS3Bucket" } as unknown) as ConfigManagerInstance
+    ).getAssetUrl("12304", "asset/name.jpg");
+    expect(mockS3.getSignedUrl).toHaveBeenCalledWith("getObject", {
+      Bucket: "editorS3Bucket",
+      Key: "12304/asset/name.jpg",
+      Expires: 3600,
+    });
+  });
+});
