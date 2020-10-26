cd94c9fb53601f37dd95b7dcb8474f235db33aef
noris alsaadie <>
2020-10-26 17:07:02 +0000

    Name service

--- src/services/asset.ts
+++ src/services/asset.ts
@@ -1,7 +1,11 @@
 import { S3 } from "aws-sdk";
 import { configManager } from "./config-manager";
 
-async function checkFileExists(s3: S3, key: string, bucket: string): Promise<boolean> {
+async function checkFileExists(
+  s3: S3,
+  key: string,
+  bucket: string
+): Promise<boolean> {
   try {
     await s3
       .headObject({
@@ -18,21 +22,23 @@ async function checkFileExists(s3: S3, key: string, bucket: string): Promise<boo
   }
 }
 
-export default (s3: S3, config: typeof configManager) => ({
-  getAssetUrl: async (
-    articleId: string,
-    fileKey: string
-  ): Promise<string | null> => {
-    const key = `${articleId}/${fileKey}`;
-    const bucket = config.get("editorS3Bucket");
-    const exists = await checkFileExists(s3, key, bucket);
-    if (!exists) {
-      return null;
-    }
-    return s3.getSignedUrl("getObject", {
-      Bucket: bucket,
-      Key: key,
-      Expires: 3600,
-    });
-  },
-});
+export default function assetService(s3: S3, config: typeof configManager) {
+  return {
+    getAssetUrl: async (
+      articleId: string,
+      fileKey: string
+    ): Promise<string | null> => {
+      const key = `${articleId}/${fileKey}`;
+      const bucket = config.get("editorS3Bucket");
+      const exists = await checkFileExists(s3, key, bucket);
+      if (!exists) {
+        return null;
+      }
+      return s3.getSignedUrl("getObject", {
+        Bucket: bucket,
+        Key: key,
+        Expires: 3600,
+      });
+    },
+  };
+}
