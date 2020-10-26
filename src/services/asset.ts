23ecec5415a5d0ac25e042db6436ba1f10f4fe92
noris alsaadie <>
2020-10-26 16:27:48 +0000

    more testing

--- src/services/asset.ts
+++ src/services/asset.ts
@@ -1,12 +1,38 @@
-import { S3 } from 'aws-sdk';
+import { S3 } from "aws-sdk";
 import { configManager } from "./config-manager";
 
-export default (s3: S3, config: typeof configManager) => ({
-  getAssetUrl: (articleId: string, fileKey: string): string => {
-    return s3.getSignedUrl('getObject', {
-        Bucket: config.get('editorS3Bucket'),
-        Key: `${articleId}/${fileKey}`,
-        Expires: 3600
-    })
+async function checkFileExists(s3: S3, key: string, bucket: string) {
+  try {
+    await s3
+      .headObject({
+        Bucket: bucket,
+        Key: key,
+      })
+      .promise();
+    return true;
+  } catch (error) {
+    if (error && error.code === "NotFound") {
+      return false;
+    }
+    throw error;
   }
+}
+
+export default (s3: S3, config: typeof configManager) => ({
+  getAssetUrl: async (
+    articleId: string,
+    fileKey: string
+  ): Promise<string | null> => {
+    const key = `${articleId}/${fileKey}`;
+    const bucket = config.get("editorS3Bucket");
+    const exists = await checkFileExists(s3, key, bucket);
+    if (!exists) {
+      return null;
+    }
+    return s3.getSignedUrl("getObject", {
+      Bucket: bucket,
+      Key: key,
+      Expires: 3600,
+    });
+  },
 });
--- test/functional/test-asset.spec.ts
+++ test/functional/test-asset.spec.ts
@@ -7,8 +7,37 @@ const agent = request.agent(API_URL);
 describe('Get /assets', () => {
   test('Returns 404 for assets that are not found', async () => {
     return agent
-      .get('/article/00000/assets/1.jpg')
+      .get('/articles/00000/assets/1.jpg')
       .set('Accept', 'application/json')
       .expect(404);
+  });
+
+  test('Should return 301 for assets that are found - tiff', async () => {
+    return agent
+      .get('/articles/54296/assets/elife-54296-fig1.tif')
+      .set('Accept', 'application/json')
+      .expect(301);
+  });
+
+  test('Should return 301 for assets that are found - jpg', async () => {
+    return agent
+      .get('/articles/54296/assets/elife-54296-fig1.jpg')
+      .set('Accept', 'application/json')
+      .expect(301);
+  });
+
+  test('Should return 301 for assets that are found - pdf', async () => {
+    return agent
+      .get('/articles/54296/assets/elife-54296.pdf')
+      .set('Accept', 'application/json')
+      .expect(301);
+  });
+
+  // perphas this shouldn't be possible
+  test('Should return 301 for assets that are found - xml', async () => {
+    return agent
+      .get('/articles/54296/assets/elife-54296.xml')
+      .set('Accept', 'application/json')
+      .expect(301);
   });
 });
