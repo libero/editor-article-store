cc17bc97736743a7a58990e8be5115bd0d6dce65
Ilya Zhuravel <ilya.zhuravel@gmail.com>
2021-01-26 15:47:59 +0000

    test: fix tests

--- test/unit/change-service.test.ts
+++ test/unit/change-service.test.ts
@@ -60,8 +60,8 @@ describe("articleService", () => {
         },
       ],
     };
-    getMock = jest.fn().mockReturnValue([change]);
+    getMock = jest.fn().mockReturnValue({ total: 1, changes: [change] });
     const changes = await changeService(db).getChangesforArticle("1234", 0);
-    expect(changes).toEqual([change]);
+    expect(changes).toEqual({ total: 1, changes: [change] });
   });
 });
