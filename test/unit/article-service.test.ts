bd827d39a70b8e318cf3467b418d9922b82dd93b
Cory <c.donkin@elifesciences.org>
2021-02-10 14:43:21 +0000

    fix: article service test

--- test/unit/article-service.test.ts
+++ test/unit/article-service.test.ts
@@ -2,8 +2,9 @@ import { Db } from "mongodb";
 import { ArticleRepository } from "../../src/repositories/articles";
 import articleService from "../../src/services/article";
 
-let getByArticleIdMock = jest.fn().mockReturnValue(null);
-let getArticlesMock = jest.fn().mockReturnValue(null);
+let getByArticleIdMock = jest.fn();
+let getArticlesMock = jest.fn();
+
 const mockArticleRepo = {
   insert: jest.fn(),
   getByArticleId: getByArticleIdMock,
@@ -15,6 +16,8 @@ describe("articleService", () => {
 
   beforeEach(async () => {
     jest.restoreAllMocks();
+    getByArticleIdMock.mockImplementation(() => null);
+    getArticlesMock.mockImplementation(() => null);
   });
 
   test("should not throw", () => {
@@ -24,7 +27,6 @@ describe("articleService", () => {
   });
 
   test("Returns null if article is not found", async () => {
-    getByArticleIdMock = jest.fn().mockReturnValue(null);
     const result = await articleService(mockArticleRepo).findByArticleId("123");
     expect(result).toBe(null);
   });
@@ -38,27 +40,27 @@ describe("articleService", () => {
       fileName: "main.xml",
       version: "v1"
     };
-    getByArticleIdMock = jest.fn().mockReturnValue(data);
+    getByArticleIdMock.mockImplementation(() => data);
     const result = await articleService(mockArticleRepo).findByArticleId("12345");
     expect(result).toBeDefined();
     expect(result).toEqual({...data });
   });
 
   test("Returns empty array if no articles", async () => {
-    getArticlesMock = jest.fn().mockReturnValue([]);
+    getArticlesMock.mockImplementation(() => []);
     const articles = await articleService(mockArticleRepo).getArticles(0);
     expect(articles.length).toBe(0);
     expect(getArticlesMock).toBeCalledWith(0);
   });
 
-  test("Returns  array if there articles", async () => {
+  test("Returns  array if there are articles", async () => {
     const data = {
       xml: "<xml></xml",
       datatype: "xml",
       articleId: "12345",
       fileName: "main.xml",
     };
-    getArticlesMock = jest.fn().mockReturnValue([data]);
+    getArticlesMock.mockImplementation(() => [data]);
     const articles = await articleService(mockArticleRepo).getArticles(0);
     expect(articles.length).toBe(1);
     expect(articles[0]).toEqual({ ...data });
