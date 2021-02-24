4c1ef70a588bb6fd20017b747c066b7a85e7b506
Ilya Zhuravel <ilya.zhuravel@gmail.com>
2021-02-24 10:31:54 +0000

    test: additional coverage

--- test/unit/model/related-article.test.ts
+++ test/unit/model/related-article.test.ts
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-import {RelatedArticle} from "../../../src/model/related-article";
+import {createRelatedArticleState, RelatedArticle} from "../../../src/model/related-article";
 
 jest.mock('uuid', () => ({
   v4: () => 'unique_id'
@@ -28,4 +28,29 @@ describe('Related Article model', () => {
     expect(new RelatedArticle()).toMatchSnapshot();
   });
 
+  it('clones a related article', () => {
+    const article = new RelatedArticle({articleType: 'SOME_ARTICLE_TYPE', href: 'URL'});
+    const clonedArticle = article.clone();
+    expect(clonedArticle).not.toBe(article);
+    expect(clonedArticle).toEqual(article);
+  });
+
+  it('creates related article state', () => {
+    const el = document.createElement('div');
+    el.innerHTML = `
+        <related-article related-article-type="commentary-article" xlink:href="10.7554/eLife.48496"/>
+        <related-article related-article-type="commentary-article" xlink:href="10.7554/eLife.48497"/>
+        <related-article related-article-type="commentary-article" xlink:href="10.7554/eLife.48498"/>
+    `;
+    const state =createRelatedArticleState(Array.from(el.querySelectorAll('related-article')));
+    expect(state.length).toBe(3);
+    expect(state[0].href).toBe('10.7554/eLife.48496');
+    expect(state[1].href).toBe('10.7554/eLife.48497');
+    expect(state[2].href).toBe('10.7554/eLife.48498');
+  });
+
+  it('creates empty state', () => {
+    const state = createRelatedArticleState([]);
+    expect(state.length).toBe(0);
+  });
 });
