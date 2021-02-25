af94a94f7c1a4031ebccef70ba8c0b64f28cd47b
Ilya Zhuravel <ilya.zhuravel@gmail.com>
2021-02-25 13:17:12 +0000

    feat: update object change class

---
+++ src/model/history/update-object-change.ts
@@ -0,0 +1,103 @@
+import * as deepDiff from 'deep-diff';
+import {get, set} from 'lodash';
+import {EditorState, Transaction} from 'prosemirror-state';
+import {Change} from './change';
+import {BatchChange} from "./batch-change";
+import {JSONObject} from '../types';
+import {ProsemirrorChange} from './prosemirror-change';
+import {Manuscript} from "../manuscript";
+import {cloneManuscript} from '../changes.utils';
+
+export class UpdateObjectChange<T> extends Change {
+  private constructor(private path: string, private differences: deepDiff.Diff<T, T>[] = []) {
+    super();
+  }
+
+  get isEmpty(): boolean {
+    return !Array.isArray(this.differences) || this.differences.length === 0;
+  }
+
+  public static createFromTwoObjects<T>(path: string, oldObject: T, newObject: T): Change {
+    const editorStateProps: string[] = [];
+
+    const pojoDifferences = deepDiff.diff(
+      oldObject,
+      newObject,
+      // if prefilter function returns true diff stops drilling down
+      (path: Array<string | number>, key: string | number) => {
+        const propPath = [...path, key].join('.');
+        if (get(oldObject, propPath) instanceof EditorState) {
+          editorStateProps.push(propPath);
+          return true;
+        }
+        return false;
+      }
+    );
+
+    const objectChange = new UpdateObjectChange(path, pojoDifferences);
+    const prosemirrorChanges = editorStateProps
+      .map((propPath: string) => {
+        const transaction = UpdateObjectChange.getEditorStatesDiff(get(oldObject, propPath), get(newObject, propPath));
+        if (transaction.docChanged) {
+          return new ProsemirrorChange(`${path}.${propPath}`, transaction);
+        }
+        return undefined;
+      })
+      .filter(Boolean) as ProsemirrorChange[];
+
+    return new BatchChange([objectChange, ...prosemirrorChanges]);
+  }
+
+  public static fromJSON<T>(data: JSONObject): UpdateObjectChange<T> {
+    const change = new UpdateObjectChange(data.path as string, (data.differences as unknown) as deepDiff.Diff<T, T>[]);
+    change._timestamp = data.timestamp as number;
+    return change;
+  }
+
+  private static getEditorStatesDiff(prevState: EditorState, nextState: EditorState): Transaction {
+    const transaction = prevState.tr;
+    const start = nextState.doc.content.findDiffStart(prevState.doc.content);
+    if (start !== null && start !== undefined) {
+      let {a: endA, b: endB} = nextState.doc.content.findDiffEnd(get(prevState, 'doc.content')) as { a: number; b: number };
+      const overlap = start - Math.min(endA, endB);
+      if (overlap > 0) {
+        endA += overlap;
+        endB += overlap;
+      }
+      transaction.replace(start, endB, nextState.doc.slice(start, endA));
+    }
+    return transaction;
+  }
+
+  applyChange(manuscript: Manuscript): Manuscript {
+    const originalSection = get(manuscript, this.path);
+    const updatedSection = this.differences.reduce((acc: T, diff) => {
+      const newAcc = cloneManuscript(acc) as T;
+      deepDiff.applyChange(newAcc, acc, diff);
+      return newAcc;
+    }, originalSection);
+
+    return set(cloneManuscript(manuscript), this.path, updatedSection);
+  }
+
+  rollbackChange(manuscript: Manuscript): Manuscript {
+    const originalSection = get(manuscript, this.path);
+
+    const updatedSection = this.differences.reduce((acc: T, diff) => {
+      const newAcc = cloneManuscript(acc);
+      deepDiff.revertChange(newAcc, acc, diff);
+      return newAcc;
+    }, originalSection);
+
+    return set(cloneManuscript(manuscript), this.path, updatedSection);
+  }
+
+  toJSON(): JSONObject {
+    return {
+      type: 'update-object',
+      timestamp: this.timestamp,
+      path: this.path,
+      differences: (this.differences as unknown) as JSONObject
+    };
+  }
+}
---
+++ test/unit/model/history/update-object-change.test.ts
@@ -0,0 +1,151 @@
+import {get} from 'lodash';
+import {UpdateObjectChange} from '../../../../src/model/history/update-object-change'
+import {Manuscript} from '../../../../src/model/manuscript';
+import {createTitleState} from "../../../../src/model/title";
+import {TextSelection} from "prosemirror-state";
+
+describe('UpdateObjectChange', () => {
+  describe('factory methods', () => {
+    it('does not throw when constructor is passed expected params', () => {
+      expect(() => UpdateObjectChange.createFromTwoObjects('somepath', {}, {prop: 'value'})).not.toThrow();
+    });
+
+    it('does creates a batch change with prosemirror change when object has EditorState changes', () => {
+      const editorState = createTitleState();
+      const obj1 = {
+        prop1: { subProp1: 'a' },
+        prop2: editorState
+      };
+      const obj2 = {
+        prop1: { subProp1: 'abc' },
+        prop2: editorState.apply(editorState.tr.insertText('Some text'))
+      };
+
+      const updateObjJSON = UpdateObjectChange.createFromTwoObjects('somepath', obj1, obj2).toJSON();
+      expect(updateObjJSON.type).toBe('batch');
+      expect(get(updateObjJSON, 'changes.length')).toBe(2);
+      expect(get(updateObjJSON, 'changes.0.type')).toBe('update-object');
+      expect(get(updateObjJSON, 'changes.1.type')).toBe('prosemirror');
+      expect(get(updateObjJSON, 'changes.0.timestamp')).toBeDefined();
+      expect(get(updateObjJSON, 'changes.1.timestamp')).toBeDefined();
+      expect(get(updateObjJSON, 'changes.0.path')).toBe('somepath');
+      expect(get(updateObjJSON, 'changes.1.path')).toBe('somepath.prop2');
+      expect(get(updateObjJSON, 'changes.0.differences')).toEqual([{
+        kind: "E",
+        lhs: "a",
+        path: ["prop1", "subProp1"],
+        rhs: "abc"
+      }]);
+
+      expect(get(updateObjJSON, 'changes.1.transactionSteps')).toEqual([{
+        from: 1,
+        slice: {
+          content: [{text: 'Some text', type: 'text'}]
+        },
+        stepType: 'replace',
+        to: 1
+      }]);
+    });
+
+    it('does not creates a prosemirror change when object has no EditorState changes', () => {
+      const editorState = createTitleState();
+      const obj1 = {
+        prop1: { subProp1: 'a' },
+        prop2: editorState.apply(editorState.tr.insertText('Some text'))
+      };
+      const obj2 = {
+        prop1: { subProp1: 'abc' },
+        prop2: obj1.prop2.apply(obj1.prop2.tr.setSelection(TextSelection.create(obj1.prop2.doc, 2,4)))
+      };
+
+      const updateObjJSON = UpdateObjectChange.createFromTwoObjects('somepath', obj1, obj2).toJSON();
+      expect(updateObjJSON.type).toBe('batch');
+      expect(get(updateObjJSON, 'changes.length')).toBe(1);
+      expect(get(updateObjJSON, 'changes.0.type')).toBe('update-object');
+      expect(get(updateObjJSON, 'changes.0.timestamp')).toBeDefined();
+      expect(get(updateObjJSON, 'changes.0.path')).toBe('somepath');
+      expect(get(updateObjJSON, 'changes.1.path')).toBe('somepath.prop2');
+      expect(get(updateObjJSON, 'changes.0.differences')).toEqual([{
+        kind: "E",
+        lhs: "a",
+        path: ["prop1", "subProp1"],
+        rhs: "abc"
+      }]);
+    });
+  });
+  describe('fromJSON', () => {
+    it('Returns UpdateObjectChange obj from passed JSON', () => {
+      const mockChangeJSON = {
+        "type": "update-object",
+        "timestamp": 1614250395860,
+        "path": "relatedArticles.0",
+        "differences": [{"kind": "E", "path": ["href"], "lhs": "10.7554/eLife.48498", "rhs": "10.7554/eLife.4849811"}]
+      };
+      expect(() => UpdateObjectChange.fromJSON(mockChangeJSON)).not.toThrow();
+      const updateObjChange = UpdateObjectChange.fromJSON(mockChangeJSON);
+      expect(updateObjChange).toBeInstanceOf(UpdateObjectChange);
+      expect(updateObjChange.toJSON()).toEqual(mockChangeJSON);
+    });
+  });
+  describe('toJSON', () => {
+    it('returns expected JSON', () => {
+      const updateObjChange = UpdateObjectChange.createFromTwoObjects('somepath', {prop: 'oldValue'}, {prop: 'value'});
+      const updateObjJSON = updateObjChange.toJSON();
+      expect(get(updateObjJSON, 'changes.0.differences')).toEqual([{
+        kind: "E",
+        lhs: "oldValue",
+        path: ["prop"],
+        rhs: "value"
+      }]);
+      expect(updateObjJSON.timestamp).toBeDefined();
+      expect(updateObjJSON.type).toBe('batch');
+      expect(get(updateObjJSON, 'changes.0.type')).toBe('update-object');
+      expect(get(updateObjJSON, 'changes.0.timestamp')).toBeDefined();
+      expect(get(updateObjJSON, 'changes.0.path')).toBe('somepath');
+    });
+  });
+  describe('applyChange', () => {
+    // TODO: update this when / if implimented
+    it('returns Manuscript', () => {
+      const updateObjChange = UpdateObjectChange.createFromTwoObjects('somepath', {}, {prop: 'value'});
+      const manuscript = {} as unknown as Manuscript;
+      expect(updateObjChange.applyChange(manuscript)).toEqual(manuscript);
+    })
+  });
+  describe('rollbackChange', () => {
+    // TODO: update this when / if implimented
+    it('returns Manuscript', () => {
+      const updateObjChange = UpdateObjectChange.createFromTwoObjects('somepath', {}, {prop: 'value'});
+      const manuscript = {} as unknown as Manuscript;
+      expect(updateObjChange.rollbackChange(manuscript)).toEqual(manuscript);
+    })
+  });
+  describe('isEmpty', () => {
+    // TODO: update this when / if implimented
+    it('returns false when differences array is not empty', () => {
+      const updateObjChange = UpdateObjectChange.createFromTwoObjects('somepath', {}, {prop: 'value'});
+      expect(updateObjChange.isEmpty).toBe(false);
+    });
+
+    it('returns when differences array is empty', () => {
+      const mockChangeJSON = {
+        "type": "update-object",
+        "timestamp": 1614250395860,
+        "path": "relatedArticles.0",
+        "differences": []
+      };
+      const updateObjChange = UpdateObjectChange.fromJSON(mockChangeJSON);
+      expect(updateObjChange.isEmpty).toBe(true);
+    });
+
+    it('returns when differences are not defined', () => {
+      const mockChangeJSON = {
+        "type": "update-object",
+        "timestamp": 1614250395860,
+        "path": "relatedArticles.0"
+      };
+      const updateObjChange = UpdateObjectChange.fromJSON(mockChangeJSON);
+      expect(updateObjChange.isEmpty).toBe(true);
+    });
+  });
+});
