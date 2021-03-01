3717384951806404a8458d4157f86c2dfd9f39df
Cory <c.donkin@elifesciences.org>
2021-03-01 14:48:40 +0000

    feat: create affiliation from json object

--- test/unit/model/affiliation.test.ts
+++ test/unit/model/affiliation.test.ts
@@ -11,4 +11,29 @@ describe('Affiliation', () => {
       address: { city: '' }
     }));
   });
+  describe('fromJSON', () => {
+    const mockData = {
+      label: 'label',
+      institution: {
+        name: 'eLife'
+      },
+      address: {
+        city: 'Cambridge'
+      },
+      country: 'UK'
+    };
+    it('creates an affiliation with specified data', () => {  
+      const affiliation = new Affiliation(mockData);
+      expect(affiliation).toBeInstanceOf(Affiliation);
+      expect(affiliation.id).toBeDefined();
+      expect(affiliation).toStrictEqual(expect.objectContaining(mockData));
+    });
+  
+    it('creates an affiliation with specified data and ID', () => {
+      const affiliation = new Affiliation({...mockData, _id: 'SOME_ID' });
+      expect(affiliation).toBeInstanceOf(Affiliation);
+      expect(affiliation.id).toBe('SOME_ID');
+      expect(affiliation).toStrictEqual(expect.objectContaining(mockData));
+    });
+  });
 });
