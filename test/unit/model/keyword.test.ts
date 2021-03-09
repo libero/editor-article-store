30ab7ed715eef7388a6344f01860f822e9d9be87
Cory <c.donkin@elifesciences.org>
2021-03-09 15:47:11 +0000

    feat: create keyword from xml (#80)
    * feat: keyword class with createBlank implemented
    
    * feat: create keyword from xml
    
    * test: make keyword assertions snapshots

--- test/unit/model/keyword.test.ts
+++ test/unit/model/keyword.test.ts
@@ -1,3 +1,6 @@
+/**
+ * @jest-environment jsdom
+ */
 import { Keyword } from '../../../src/model/keyword';
 
 jest.mock('uuid', () => ({
@@ -7,6 +10,93 @@ jest.mock('uuid', () => ({
 describe('Keyword', () => {
   it('returns empty Keyword when called with no data', () => {
     const keyword = new Keyword();
-    expect(JSON.stringify(keyword)).toMatch("{\"_id\":\"unique_id\",\"content\":{\"doc\":{\"type\":\"keyword\"},\"selection\":{\"type\":\"text\",\"anchor\":0,\"head\":0}}}");
+    expect(keyword).toMatchInlineSnapshot(`
+    Keyword {
+      "_id": "unique_id",
+      "content": Object {
+        "doc": Object {
+          "type": "keyword",
+        },
+        "selection": Object {
+          "anchor": 0,
+          "head": 0,
+          "type": "text",
+        },
+      },
+    }
+    `);
+  });
+  describe('fromXml', () => {
+    it('returns empty Keyword when called with empty aff xml fragment', () => {
+      const keyword = new Keyword(document.createElement('kwd-group'));
+      expect(keyword).toMatchInlineSnapshot(`
+      Keyword {
+        "_id": "unique_id",
+        "content": Object {
+          "doc": Object {
+            "type": "keyword",
+          },
+          "selection": Object {
+            "anchor": 0,
+            "head": 0,
+            "type": "text",
+          },
+        },
+      }
+      `);
+    });
+    it('creates a Keyword from a complete xml fragment', () => {
+      const element = document.createElement('kwd')
+      element.innerHTML = `A<sub>Poodle</sub><italic>Puppy</italic><sup>Dog</sup>`;
+      const keyword = new Keyword(element);
+      expect(keyword).toMatchInlineSnapshot(`
+      Keyword {
+        "_id": "unique_id",
+        "content": Object {
+          "doc": Object {
+            "content": Array [
+              Object {
+                "text": "A",
+                "type": "text",
+              },
+              Object {
+                "marks": Array [
+                  Object {
+                    "type": "subscript",
+                  },
+                ],
+                "text": "Poodle",
+                "type": "text",
+              },
+              Object {
+                "marks": Array [
+                  Object {
+                    "type": "italic",
+                  },
+                ],
+                "text": "Puppy",
+                "type": "text",
+              },
+              Object {
+                "marks": Array [
+                  Object {
+                    "type": "superscript",
+                  },
+                ],
+                "text": "Dog",
+                "type": "text",
+              },
+            ],
+            "type": "keyword",
+          },
+          "selection": Object {
+            "anchor": 0,
+            "head": 0,
+            "type": "text",
+          },
+        },
+      }
+      `);
+    });
   });
 });
