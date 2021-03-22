c5aa65cf58db668fc180d5609f705d3865706af6
Ilya Zhuravel <ilya.zhuravel@gmail.com>
2021-03-22 08:06:27 +0000

    feat: software reference

--- src/model/reference/SoftwareReference.ts
+++ src/model/reference/SoftwareReference.ts
@@ -4,44 +4,48 @@ import { JSONObject } from "../types";
 import { getTextContentFromPath } from "../utils";
 import { createReferenceAnnotatedValue, deserializeReferenceAnnotatedValue } from "./reference.utils";
 
-export class PeriodicalReference extends BackmatterEntity {
-  date!: string;
+export class SoftwareReference extends BackmatterEntity {
+  year!: string;
   source!: EditorState;
   articleTitle!: EditorState;
-  volume!: string;
-  firstPage!: string;
-  lastPage!: string;
+  version!: string;
+  publisherName!: string;
+  publisherLocation!: string;
   extLink!: string;
-  
+  doi!: string;
+
   constructor(data?: Element | JSONObject) {
     super();
     this.createEntity(data)
   }
   protected fromJSON(json: JSONObject) {
     this._id = json._id as string || this._id;
+    this.year = json.year as string || '';
+    this.doi = json.doi as string || '';
+    this.version = json.version as string || '';
+    this.publisherLocation = json.publisherLocation as string || '';
+    this.publisherName = json.publisherName as string || '';
     this.articleTitle = json.articleTitle ? deserializeReferenceAnnotatedValue(json.articleTitle as JSONObject) : createReferenceAnnotatedValue();
-    this.date = json.date as string || '';
-    this.firstPage = json.firstPage as string || '';
-    this.lastPage = json.lastPage as string || '';
-    this.volume = json.volume as string || '';
     this.extLink = json.extLink as string || '';
     this.source = json.source ? deserializeReferenceAnnotatedValue(json.source as JSONObject) : createReferenceAnnotatedValue();
   }
   protected fromXML(referenceXml: Element) {
-    this.date = referenceXml.querySelector('string-date > year')?.getAttribute('iso-8601-date') || '';
-    this.source = createReferenceAnnotatedValue(referenceXml.querySelector('source'));
+    this.year = getTextContentFromPath(referenceXml, 'year');
     this.articleTitle = createReferenceAnnotatedValue(referenceXml.querySelector('article-title'));
-    this.firstPage = getTextContentFromPath(referenceXml, 'fpage') || '';
-    this.lastPage = getTextContentFromPath(referenceXml, 'lpage') || '';
+    this.source = createReferenceAnnotatedValue(referenceXml.querySelector('source'));
+    this.version = getTextContentFromPath(referenceXml, 'version') || '';
+    this.publisherLocation = getTextContentFromPath(referenceXml, 'publisher-loc') || '';
+    this.publisherName = getTextContentFromPath(referenceXml, 'publisher-name') || '';
+    this.doi = getTextContentFromPath(referenceXml, 'pub-id[pub-id-type="doi"]') || '';
     this.extLink = getTextContentFromPath(referenceXml, 'ext-link') || '';
-    this.volume = getTextContentFromPath(referenceXml, 'volume') || '';
   }
   protected createBlank() {
+    this.year = '';
+    this.doi = '';
+    this.version = '';
+    this.publisherLocation = '';
+    this.publisherName =  '';
     this.articleTitle = createReferenceAnnotatedValue();
-    this.date = '';
-    this.firstPage = '';
-    this.lastPage = '';
-    this.volume = '';
     this.extLink = '';
     this.source = createReferenceAnnotatedValue();
   }
---
+++ test/unit/model/reference/SoftwareReference.test.ts
@@ -0,0 +1,126 @@
+import {SoftwareReference} from '../../../../src/model/reference/SoftwareReference';
+import {parseXML} from '../../../../src/xml-exporter/xml-utils';
+
+jest.mock('uuid', () => ({
+  v4: () => 'unique_id'
+}));
+
+const emptySoftwareRefJSON = {
+  "doi": "",
+  "publisherLocation": "",
+  "publisherName": "",
+  "version": "",
+  "extLink": "",
+  "year": "",
+};
+
+const populatedSoftwareRefJSON = {
+  "doi": "doi",
+  "publisherLocation": "publisherLocation",
+  "publisherName": "publisherName",
+  "version": "version",
+  "extLink": "extLink",
+  "year": "year"
+};
+
+describe('SoftwareReference', () => {
+  it('creates a blank SoftwareReference when passed no constructor args', () => {
+    const softwareReference = new SoftwareReference();
+    expect(softwareReference).toEqual(expect.objectContaining(emptySoftwareRefJSON));
+    expect(softwareReference.articleTitle.doc.textContent).toBe("");
+    expect(softwareReference.source.doc.textContent).toBe("");
+    expect(softwareReference.id).toBe("unique_id");
+  });
+  describe('fromJSON', () => {
+    it('returns empty SoftwareReference when called with empty data object', () => {
+      const softwareReference = new SoftwareReference({});
+      expect(softwareReference).toEqual(expect.objectContaining(emptySoftwareRefJSON));
+      expect(softwareReference.articleTitle.doc.textContent).toBe("");
+      expect(softwareReference.source.doc.textContent).toBe("");
+      expect(softwareReference.id).toBe("unique_id");
+    });
+    it('returns SoftwareReference when called with populated data object ', () => {
+      const softwareReference = new SoftwareReference({
+        ...populatedSoftwareRefJSON,
+        "articleTitle": {
+          "doc": {
+            "content": [
+              {
+                "text": "I am articleTitle text",
+                "type": "text",
+              },
+            ],
+            "type": "annotatedReferenceInfoDoc",
+          },
+          "selection": {
+            "anchor": 0,
+            "head": 0,
+            "type": "text",
+          },
+        },
+        "source": {
+          "doc": {
+            "content": [
+              {
+                "text": "I am source text",
+                "type": "text",
+              },
+            ],
+            "type": "annotatedReferenceInfoDoc",
+          },
+          "selection": {
+            "anchor": 0,
+            "head": 0,
+            "type": "text",
+          },
+        }
+      });
+      expect(softwareReference).toEqual(expect.objectContaining(populatedSoftwareRefJSON));
+      expect(softwareReference.articleTitle.doc.textContent).toBe("I am articleTitle text");
+      expect(softwareReference.source.doc.textContent).toBe("I am source text");
+      expect(softwareReference.id).toBe("unique_id");
+    });
+
+    it('creates an SoftwareReference with specified data and ID', () => {
+      const softwareReference = new SoftwareReference({...populatedSoftwareRefJSON, _id: 'SOME_ID'});
+      expect(softwareReference.id).toBe('SOME_ID');
+      expect(softwareReference).toStrictEqual(expect.objectContaining(populatedSoftwareRefJSON));
+    });
+  });
+  describe('fromXml', () => {
+    it('returns empty SoftwareReference when called with empty XML fragment', () => {
+      const xmlWrapper = parseXML(`<article><element-citation /></article>`);
+      const softwareReference = new SoftwareReference(xmlWrapper.querySelector('element-citation') as Element);
+      expect(softwareReference).toEqual(expect.objectContaining(emptySoftwareRefJSON));
+      expect(softwareReference.articleTitle.doc.textContent).toBe("");
+      expect(softwareReference.source.doc.textContent).toBe("");
+      expect(softwareReference.id).toBe("unique_id");
+    });
+    it('returns SoftwareReference when called with populated XML fragment', () => {
+      const xmlWrapper = parseXML(`<article><element-citation>
+          <year iso-8601-date="2019">2019</year>
+          <article-title>An articleTitle</article-title>
+          <source>A source Title</source>
+          <version designator="3e48e64">3e48e64</version>
+          <pub-id pub-id-type="doi">10.7554/eLife.00067</pub-id>
+          <publisher-name>GitHub</publisher-name>
+          <publisher-loc>Github Land</publisher-loc>
+          <ext-link ext-link-type="uri">https://github.com/danilinares/2018LinaresAguilarLopezmoliner</ext-link>
+        </element-citation></article>`);
+
+      const softwareReference = new SoftwareReference(xmlWrapper.querySelector('element-citation') as Element);
+      expect(softwareReference).toEqual(expect.objectContaining({
+        "doi": "10.7554/eLife.00067",
+        "year": "2019",
+        "publisherLocation": "Github Land",
+        "publisherName": "GitHub",
+        "version": "3e48e64",
+        "extLink": "https://github.com/danilinares/2018LinaresAguilarLopezmoliner"
+      }));
+      expect(softwareReference.articleTitle.doc.textContent).toBe("An articleTitle" +
+        "");
+      expect(softwareReference.source.doc.textContent).toBe("A source Title");
+      expect(softwareReference.id).toBe("unique_id");
+    });
+  });
+});
