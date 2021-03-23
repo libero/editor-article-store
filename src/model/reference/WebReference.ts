67290f551c22be4692beb277c87b6e81f9fbb370
Ilya Zhuravel <ilya.zhuravel@gmail.com>
2021-03-23 12:10:31 +0000

    feat: serialize WebReference to XML

--- src/model/reference/WebReference.ts
+++ src/model/reference/WebReference.ts
@@ -1,8 +1,11 @@
-import { EditorState } from "prosemirror-state";
-import { BackmatterEntity } from "../backmatter-entity";
-import { JSONObject } from "../types";
-import { getTextContentFromPath } from "../utils";
-import { createReferenceAnnotatedValue, deserializeReferenceAnnotatedValue } from "./reference.utils";
+import {EditorState} from "prosemirror-state";
+import {BackmatterEntity} from "../backmatter-entity";
+import {JSONObject} from "../types";
+import {getTextContentFromPath} from "../utils";
+import {createReferenceAnnotatedValue, deserializeReferenceAnnotatedValue} from "./reference.utils";
+import {DOMImplementation} from "xmldom";
+import {serializeManuscriptSection} from "../../xml-exporter/manuscript-serializer";
+import moment from "moment";
 
 export class WebReference extends BackmatterEntity {
   year!: string;
@@ -10,11 +13,46 @@ export class WebReference extends BackmatterEntity {
   articleTitle!: EditorState;
   dateInCitation!: string;
   extLink!: string;
-  
+
   constructor(data?: Element | JSONObject) {
     super();
     this.createEntity(data)
   }
+
+  toXml(): DocumentFragment {
+    const xmlDoc = new DOMImplementation().createDocument(null, null);
+    const xml = xmlDoc.createDocumentFragment();
+
+    const year = xmlDoc.createElement('year');
+    year.setAttribute('iso-8601-date', this.year);
+    year.appendChild(xmlDoc.createTextNode(this.year));
+    xml.appendChild(year);
+
+    const extLink = xmlDoc.createElement('ext-link');
+    extLink.setAttribute('ext-link-type', 'uri');
+    extLink.setAttribute('xlink:href', this.extLink);
+    extLink.appendChild(xmlDoc.createTextNode(this.extLink));
+    xml.appendChild(extLink);
+
+    const articleTitle = xmlDoc.createElement('article-title');
+    articleTitle.appendChild(serializeManuscriptSection(this.articleTitle, xmlDoc));
+    xml.appendChild(articleTitle);
+
+    const source = xmlDoc.createElement('source');
+    source.appendChild(serializeManuscriptSection(this.source, xmlDoc));
+    xml.appendChild(source);
+
+    if (this.dateInCitation) {
+      const dateInCitation = xmlDoc.createElement('date-in-citation');
+      dateInCitation.setAttribute('iso-8601-date', this.dateInCitation);
+      const date = moment(this.dateInCitation);
+      dateInCitation.appendChild(xmlDoc.createTextNode(date.format('MMMM D, YYYY')));
+      xml.appendChild(dateInCitation)
+    }
+
+    return xml;
+  }
+
   protected fromJSON(json: JSONObject) {
     this._id = json._id as string || this._id;
     this.articleTitle = json.articleTitle ? deserializeReferenceAnnotatedValue(json.articleTitle as JSONObject) : createReferenceAnnotatedValue();
@@ -23,6 +61,7 @@ export class WebReference extends BackmatterEntity {
     this.extLink = json.extLink as string || '';
     this.source = json.source ? deserializeReferenceAnnotatedValue(json.source as JSONObject) : createReferenceAnnotatedValue();
   }
+
   protected fromXML(referenceXml: Element) {
     this.year = getTextContentFromPath(referenceXml, 'year') || '';
     this.source = createReferenceAnnotatedValue(referenceXml.querySelector('source'));
@@ -31,6 +70,7 @@ export class WebReference extends BackmatterEntity {
     const dateInCitationEl = referenceXml.querySelector('date-in-citation');
     this.dateInCitation = dateInCitationEl ? dateInCitationEl.getAttribute('iso-8601-date') || '' : '';
   }
+
   protected createBlank() {
     this.articleTitle = createReferenceAnnotatedValue();
     this.source = createReferenceAnnotatedValue();
