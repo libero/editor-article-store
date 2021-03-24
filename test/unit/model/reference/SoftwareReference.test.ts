9250568614d5e561b1e0850ca9e25a0aaa6e4bd8
Ilya Zhuravel <ilya.zhuravel@gmail.com>
2021-03-24 17:11:51 +0000

    feat: serialize software referenceto xml

--- test/unit/model/reference/SoftwareReference.test.ts
+++ test/unit/model/reference/SoftwareReference.test.ts
@@ -1,5 +1,6 @@
 import {SoftwareReference} from '../../../../src/model/reference/SoftwareReference';
 import {parseXML} from '../../../../src/xml-exporter/xml-utils';
+import * as xmldom from "xmldom";
 
 jest.mock('uuid', () => ({
   v4: () => 'unique_id'
@@ -122,5 +123,57 @@ describe('SoftwareReference', () => {
       expect(softwareReference.source.doc.textContent).toBe("A source Title");
       expect(softwareReference.id).toBe("unique_id");
     });
+  });
+
+  describe('toXml', () => {
+    const xmlSerializer = new xmldom.XMLSerializer();
+
+    it('should serialize an empty software reference', () => {
+      const reference = new SoftwareReference(emptySoftwareRefJSON);
+      const xmlString = xmlSerializer.serializeToString(reference.toXml());
+      expect(xmlString)
+        .toBe('<element-citation publication-type="software"><year iso-8601-date=""></year><ext-link ext-link-type="uri" xlink:href=""></ext-link><article-title/><source/><pub-id pub-id-type="doi"></pub-id><version></version><publisher-name></publisher-name><publisher-loc></publisher-loc></element-citation>');
+    });
+
+    it('should serialize a populated software reference', () => {
+      const reference = new SoftwareReference({ ...populatedSoftwareRefJSON,
+        "articleTitle": {
+          "doc": {
+            "content": [
+              {
+                "text": "I am articleTitle text",
+                "type": "text",
+              },
+            ],
+            "type": "annotatedReferenceInfoDoc",
+          },
+          "selection": {
+            "anchor": 0,
+            "head": 0,
+            "type": "text",
+          },
+        },
+        "source": {
+          "doc": {
+            "content": [
+              {
+                "text": "I am source text",
+                "type": "text",
+              },
+            ],
+            "type": "annotatedReferenceInfoDoc",
+          },
+          "selection": {
+            "anchor": 0,
+            "head": 0,
+            "type": "text",
+          },
+        }
+      });
+      const xmlString = xmlSerializer.serializeToString(reference.toXml());
+      expect(xmlString)
+        .toBe('<element-citation publication-type="software"><year iso-8601-date="year">year</year><ext-link ext-link-type="uri" xlink:href="extLink">extLink</ext-link><article-title>I am articleTitle text</article-title><source>I am source text</source><pub-id pub-id-type="doi">doi</pub-id><version>version</version><publisher-name>publisherName</publisher-name><publisher-loc>publisherLocation</publisher-loc></element-citation>');
+    });
+
   });
 });
