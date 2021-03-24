f5f7a513f893b03ae80a190da2de47654de5e84e
Ilya Zhuravel <ilya.zhuravel@gmail.com>
2021-03-24 18:47:24 +0000

    747 report ref to xml (#111)
    * feat: serialize DataReference to XML
    
    * feat: serialize ReportReference to XML
    
    * fix: add missing publication type
    
    * fix: pr feedback

--- src/model/reference/ReportReference.ts
+++ src/model/reference/ReportReference.ts
@@ -6,6 +6,8 @@ import {
   createReferenceAnnotatedValue,
   deserializeReferenceAnnotatedValue
 } from "./reference.utils";
+import {DOMImplementation} from "xmldom";
+import {serializeManuscriptSection} from "../..//xml-exporter/manuscript-serializer";
 
 export class ReportReference extends BackmatterEntity {
   year!: string;
@@ -23,7 +25,57 @@ export class ReportReference extends BackmatterEntity {
     this.createEntity(data);
   }
 
-  fromJSON(json: JSONObject) {
+  toXml(): Element {
+    const xmlDoc = new DOMImplementation().createDocument(null, null);
+    const xml = xmlDoc.createElement('element-citation');
+    xml.setAttribute('publication-type', 'report');
+
+    const year = xmlDoc.createElement('year');
+    year.setAttribute('iso-8601-date', this.year);
+    year.appendChild(xmlDoc.createTextNode(this.year));
+    xml.appendChild(year);
+
+    const extLink = xmlDoc.createElement('ext-link');
+    extLink.setAttribute('ext-link-type', 'uri');
+    extLink.setAttribute('xlink:href', this.extLink);
+    extLink.appendChild(xmlDoc.createTextNode(this.extLink));
+    xml.appendChild(extLink);
+
+    const source = xmlDoc.createElement('source');
+    source.appendChild(serializeManuscriptSection(this.source, xmlDoc));
+    xml.appendChild(source);
+
+    const pubName = xmlDoc.createElement('publisher-name');
+    pubName.appendChild(xmlDoc.createTextNode(this.publisherName));
+    xml.appendChild(pubName);
+
+    const pubLocation = xmlDoc.createElement('publisher-loc');
+    pubLocation.appendChild(xmlDoc.createTextNode(this.publisherLocation));
+    xml.appendChild(pubLocation);
+
+    const volume = xmlDoc.createElement('volume');
+    volume.appendChild(xmlDoc.createTextNode(this.volume));
+    xml.appendChild(volume);
+
+    const doi = xmlDoc.createElement('pub-id');
+    doi.setAttribute('pub-id-type', 'doi');
+    doi.appendChild(xmlDoc.createTextNode(this.doi));
+    xml.appendChild(doi);
+
+    const pmid = xmlDoc.createElement('pub-id');
+    pmid.setAttribute('pub-id-type', 'pmid');
+    pmid.appendChild(xmlDoc.createTextNode(this.pmid));
+    xml.appendChild(pmid);
+
+    const isbn = xmlDoc.createElement('pub-id');
+    isbn.setAttribute('pub-id-type', 'isbn');
+    isbn.appendChild(xmlDoc.createTextNode(this.isbn));
+    xml.appendChild(isbn);
+
+    return xml;
+  }
+
+  protected fromJSON(json: JSONObject) {
     this._id = (json._id as string) || this.id;
     this.isbn = json.isbn as string || '';
     this.publisherLocation = json.publisherLocation as string || '';
@@ -36,7 +88,7 @@ export class ReportReference extends BackmatterEntity {
     this.source = json.source ? deserializeReferenceAnnotatedValue(json.source as JSONObject) : createReferenceAnnotatedValue();
   }
 
-  fromXML(referenceXml: Element) {
+  protected fromXML(referenceXml: Element) {
     this.year = getTextContentFromPath(referenceXml, 'year') || '';
     this.source = createReferenceAnnotatedValue(referenceXml.querySelector('source'));
     this.publisherLocation = getTextContentFromPath(referenceXml, 'publisher-loc') || '';
@@ -48,7 +100,7 @@ export class ReportReference extends BackmatterEntity {
     this.volume = getTextContentFromPath(referenceXml, 'volume') || '';
   }
 
-  createBlank() {
+  protected createBlank() {
     this.isbn = '';
     this.publisherLocation = '';
     this.publisherName = '';
