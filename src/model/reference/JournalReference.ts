feb63ca843a40ad27542963c135e0f0e9fdc2be4
Ilya Zhuravel <ilya.zhuravel@gmail.com>
2021-03-25 13:10:19 +0000

    743 serialize journal reference (#116)
    * feat: serialize journal reference
    
    * fix: typo

--- src/model/reference/JournalReference.ts
+++ src/model/reference/JournalReference.ts
@@ -6,6 +6,8 @@ import {
   createReferenceAnnotatedValue,
   deserializeReferenceAnnotatedValue
 } from "./reference.utils";
+import {serializeManuscriptSection} from "../../xml-exporter/manuscript-serializer";
+import { DOMImplementation } from 'xmldom'
 
 export class JournalReference extends BackmatterEntity {
   year!: string;
@@ -22,6 +24,59 @@ export class JournalReference extends BackmatterEntity {
   constructor(data?: JSONObject | Element) {
     super();
     this.createEntity(data);
+  }
+
+  toXml(): Element {
+    const xmlDoc = new DOMImplementation().createDocument(null, null);
+    const xml = xmlDoc.createElement('element-citation');
+    xml.setAttribute('publication-type', 'journal');
+
+    const elocation = xmlDoc.createElement('elocation-id');
+    elocation.appendChild(xmlDoc.createTextNode(this.elocationId));
+    xml.appendChild(elocation);
+
+    const firstPage = xmlDoc.createElement('fpage');
+    firstPage.appendChild(xmlDoc.createTextNode(this.firstPage));
+    xml.appendChild(firstPage);
+
+    const lastPage = xmlDoc.createElement('lpage');
+    lastPage.appendChild(xmlDoc.createTextNode(this.lastPage));
+    xml.appendChild(lastPage);
+
+    const year = xmlDoc.createElement('year');
+    year.setAttribute('iso-8601-date', this.year);
+    year.appendChild(xmlDoc.createTextNode(this.year));
+    xml.appendChild(year);
+
+    const articleTitle = xmlDoc.createElement('article-title');
+    articleTitle.appendChild(serializeManuscriptSection(this.articleTitle, xmlDoc));
+    xml.appendChild(articleTitle);
+
+    const source = xmlDoc.createElement('source');
+    source.appendChild(serializeManuscriptSection(this.source, xmlDoc));
+    xml.appendChild(source);
+
+    const doi = xmlDoc.createElement('pub-id');
+    doi.setAttribute('pub-id-type', 'doi');
+    doi.appendChild(xmlDoc.createTextNode(this.doi));
+    xml.appendChild(doi);
+
+    const pmid = xmlDoc.createElement('pub-id');
+    pmid.setAttribute('pub-id-type', 'pmid');
+    pmid.appendChild(xmlDoc.createTextNode(this.pmid));
+    xml.appendChild(pmid);
+
+    const volume = xmlDoc.createElement('volume');
+    volume.appendChild(xmlDoc.createTextNode(this.volume));
+    xml.appendChild(volume);
+
+    if (this.inPress) {
+      const comment = xmlDoc.createElement('comment');
+      comment.appendChild(xmlDoc.createTextNode('In press'));
+      xml.appendChild(comment);
+    }
+
+    return xml;
   }
 
   fromJSON(json: JSONObject) {
