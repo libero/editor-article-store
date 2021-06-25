50dcb36d87e3292805e8fcb24d986709e1e5dcf5
Cory <c.donkin@elifesciences.org>
2021-06-25 08:47:53 +0100

    feat: remove manifest route and services

--- test/functional/test-article.spec.ts
+++ test/functional/test-article.spec.ts
@@ -52,37 +52,6 @@ describe('Get /article/id', () => {
         await populateCollection('articles', [{ ...article }]);
         return agent.get('/articles/54296').expect('Content-Type', /json/).expect(200);
     });
-
-    it('Can get article manifest', async () => {
-        await populateCollection('articles', [
-            {
-                ...article,
-                xml: '<article><body><fig id="fig1" position="float"><graphic xlink:href="elife-54296-fig1.tif" mimetype="image" mime-subtype="tiff"/></fig></body></article>',
-            },
-        ]);
-        return agent
-            .get('/articles/54296/manifest')
-            .expect('Content-Type', /json/)
-            .expect(200)
-            .then((response) => {
-                expect(response.body).toEqual({
-                    articleId: '54296',
-                    assets: [
-                        {
-                            id: 'fig1',
-                            path: 'http://localhost:8080/api/v1/articles/54296/assets/elife-54296-fig1.tif',
-                            type: 'image/jpeg',
-                        },
-                    ],
-                    path: '/api/v1/articles/54296/export',
-                    type: '',
-                });
-            });
-    });
-
-    it('Returns 404 for an invalid article manifest', async () => {
-        return agent.get('/articles/00000/manifest').expect(404);
-    });
 });
 
 describe('Get /article/id/export', () => {
