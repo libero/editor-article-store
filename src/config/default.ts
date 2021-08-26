2f46b617aff9cf677491edb879c5eb7ec3385c88
Cory <c.donkin@elifesciences.org>
2021-08-26 13:42:14 +0100

    refactor: revert config

--- src/config/default.ts
+++ src/config/default.ts
@@ -17,7 +17,7 @@ export const defaultConfig: Config = {
     editorS3Bucket: 'editor',
     srcS3Bucket: 'kryia',
     importTransformUrl: 'http://transformer:8984/v1tov2',
-    importTransformEnabled: true,
+    importTransformEnabled: false,
     exportTransformUrl: 'http://transformer:8984/v2tov1',
     exportTransformEnabled: false,
 };
